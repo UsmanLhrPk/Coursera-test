8a01c093337063875a63279a385ae2a9baf7e057
UsmanLhrPk <usmanjzcn@gmail.com>
2018-12-26 23:34:51 +0500

    Uploadiing webpage from Hamza

---
+++ Intro to Ruby and Rails/index.rb
@@ -0,0 +1,18 @@
+my_name = " tim"
+puts my_name.lstrip.capitalize # => Tim
+p my_name # => " tim"
+my_name.lstrip! # (destructive) removes the leading space
+my_name[0] = 'K' # replace the first character
+puts my_name # => Kim
+
+cur_weather = %Q{It's a hot day outside Grab your umbrella...}
+
+cur_weather.lines do |line|
+	line.sub! 'hot', 'rainy' # substitute 'hot' with 'rainy'
+	puts "#{line.strip}"
+end
+
+# => It's a rainy day outside
+# => Grab your umbrellas...
+
+:"fool of a took"
